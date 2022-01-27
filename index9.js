daacd0c244c25501a485cc06bb9fe158430ddcd4
bnurmos <yuliaslinc@gmail.com>
2022-01-27 15:44:12 -0700

    Created Ex_9

---
+++ index9.js
@@ -0,0 +1,9 @@
+const firstName = "Yulia";
+firstName = "Pablo";
+console.log(firstName);
+
+// //C:\Users\pravi\Desktop\ComIT\JS\index9.js:2
+// firstName = "Pablo";
+// ^
+
+// TypeError: Assignment to constant variable.
