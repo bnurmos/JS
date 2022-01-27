420d595ae458403fb3c3bc872bdd396c57ba38fd
bnurmos <yuliaslinc@gmail.com>
2022-01-27 15:39:44 -0700

    Created Ex_8

---
+++ index7.js
@@ -0,0 +1,10 @@
+let firstName = "Bob";
+let lastName = "Marly";
+let age = 32;
+let dateOfBirth = "12/05/1854";
+let address = "London, 1St Street 32";
+console.log('name: ', firstName);
+console.log('last name: ', lastName);
+console.log('age: ', age);
+console.log('date of birth: ', dateOfBirth);
+console.log('address: ', address);
