661490e525d105d7dc90c064f3854a45aa03426c
bnurmos <yuliaslinc@gmail.com>
2022-01-27 16:25:59 -0700

    Created Ex_12-14

---
+++ index12.js
@@ -0,0 +1,14 @@
+//Ex_12
+let firstName = 'Juan';
+let greeting = 'Welcome';
+
+console.log(firstName + ' ' + greeting);
+
+// Ex_13
+
+console.log(greeting + ' ' + firstName + ' ' + "to learning JavaScript with friends");
+
+//Ex_14
+
+let endOfMessage = "to learning JavaScript with friends";
+console.log(greeting + ' ' + firstName + ' ' + endOfMessage);
