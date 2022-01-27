8a1669358b0c71578a5cc23b3c0a16d67d9b197a
bnurmos <yuliaslinc@gmail.com>
2022-01-26 17:22:22 -0700

    Chaged numbers format to string for DoB for Ex_2

--- index2.js
+++ index2.js
@@ -3,11 +3,10 @@ var lastName;
 var age;
 var dateOfBirth;
 var address;
-
 firstName = "Bob";
 lastName = "Marly";
 age = 32;
-dateOfBirth = 12/05/1854;
+dateOfBirth = "12/05/1854";
 address = "London, 1St Street 32";
 
 
