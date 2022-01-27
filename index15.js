2966cde356d983250fdd739935c76e22a4f5ba10
bnurmos <yuliaslinc@gmail.com>
2022-01-27 16:41:18 -0700

    Created Ex_15-17

---
+++ index15.js
@@ -0,0 +1,18 @@
+//Ex_15
+let bestStudentName;
+bestStudentName = "Galushka";
+console.log(`The best class student is: ${bestStudentName}`);
+
+//Ex_16
+
+
+console.log("The best class student is:" + ' ' + bestStudentName);
+
+
+//Ex_17
+let seriesName = " Game of Thrones";
+let dragon1 = "Drogon";
+let dragon2 = "Rhaegal";
+let dragon3 = "Viserion";
+let template = `In ${seriesName} the dragons name are: ${dragon1}, ${dragon2} & ${dragon3}`;
+console.log(template);
