1bffeb02626ff342dd2517d30645c559cca81c70
bnurmos <yuliaslinc@gmail.com>
2022-01-28 16:12:35 -0700

    Created Ex_21

---
+++ index21.js
@@ -0,0 +1,7 @@
+let age = 20;
+let phone = 911;
+let streetName = "Tuscany";
+let streetNumber = 18;
+console.log(`I'm ${age} years old`);
+console.log(`My phone number is: ${phone}`);
+console.log(`I live on ${streetNumber} ${streetName}`)
