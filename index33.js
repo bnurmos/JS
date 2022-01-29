e4a1e2e1d1dfff89a33a431d54bcc573eb73be98
bnurmos <yuliaslinc@gmail.com>
2022-01-28 18:28:09 -0700

    Created Ex_33

---
+++ index33.js
@@ -0,0 +1,11 @@
+let userCount;
+userCount += 5;
+console.log(`Users Count: ${userCount}`);
+userCount -=3;
+console.log(`Users Count: ${userCount}`);
+userCount *=2;
+console.log(`Users Count: ${userCount}`);
+let menCount = userCount / 2;
+let womenCount = userCount / 2;
+console.log(`Users Count: ${userCount}, ${menCount} & ${womenCount}`);
+
