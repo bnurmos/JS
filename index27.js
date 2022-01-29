7054eae557b8604aec804dc57399033eed501801
bnurmos <yuliaslinc@gmail.com>
2022-01-28 18:04:33 -0700

    Created Ex_27

---
+++ index27.js
@@ -0,0 +1,11 @@
+let firstNumber=5, secondNumber=25;
+let addResult = firstNumber + secondNumber;
+let subtractResult = secondNumber - firstNumber;
+let multiplyResult = firstNumber * secondNumber;
+let dividResult = secondNumber / firstNumber;
+let remainResult = secondNumber%firstNumber;
+console.log(`I add ${firstNumber} + ${secondNumber} and the result is ${addResult}`);
+console.log(`I subtract ${secondNumber} - ${firstNumber} and the result is ${subtractResult}`);
+console.log(`I multiply ${firstNumber} * ${secondNumber} and the result is ${multiplyResult}`);
+console.log(`I divide  ${secondNumber} / ${firstNumber} and the result is ${dividResult}`);
+console.log(`I remain ${secondNumber} - ${firstNumber} and the result is ${remainResult}`);
