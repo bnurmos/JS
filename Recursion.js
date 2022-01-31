c87e47f650f21397b8f34cbee652701652f1c037
bnurmos <yuliaslinc@gmail.com>
2022-01-31 15:14:07 -0700

    Created Recursion

---
+++ Recursion.js
@@ -0,0 +1,20 @@
+
+function isEven(num) {
+    if (num%2==0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+  console.log(isEven(50));
+  console.log(isEven(75));
+  console.log(isEven(-1));
+ 
+  //Solution from the book::
+  function isEven(n) {
+    if (n == 0) return true;
+    else if (n == 1) return false;
+    else if (n < 0) return isEven(-n);
+    else return isEven(n - 2);
+  }
+  
