7aeae4ff77d6b5d2addb00cb8f6f86edc0a0deaa
bnurmos <yuliaslinc@gmail.com>
2022-01-30 17:08:13 -0700

    Created Ex_52-54

---
+++ index52.js
@@ -0,0 +1,30 @@
+let firstNumber, secondNumber;
+firstNumber = 10;
+secondNumber = 15;
+if (firstNumber > secondNumber){
+    console.log(`Number ${firstNumber} is bigger than ${secondNumber}`)
+}
+else {
+    console.log(`Number ${secondNumber} is bigger than ${firstNumber}`)
+}
+
+///Ex_52
+
+let number = 11;
+if (number % 2 == 0) {
+    console.log(`${number} is an even number`)
+}
+else {
+    console.log(`${number} is an odd number`) 
+}
+//Ex_53
+
+let username = "pepe2017";
+let password = 123456;
+if (username == "pepe2017" && password == 12345){
+    console.log("Logged in username, show user home page");
+} 
+else {
+    console.log("Sorry, there has been a problem, please try it again.");
+
+}
