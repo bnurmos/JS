fb274c54ce014c60851bb8ea15d2dd6b195a0fd9
bnurmos <yuliaslinc@gmail.com>
2022-01-30 20:24:56 -0700

    Created Ex_773-74

---
+++ index73.js
@@ -0,0 +1,25 @@
+// i=13;
+// a="*************";
+// while (i>0){ 
+//     console.log(a);
+//     i--;
+//     a=a.slice(0,a.length-1);
+// }
+
+a="*************";
+while(a.length >0 ) {
+    console.log(a);
+    a=a.slice(0,a.length-1);
+}
+
+//Ex_74
+
+
+// i=7;
+// step = "*";
+// while (i>0){
+    
+//     console.log(step);
+//     step = step +"**"
+//     i--;
+// }
