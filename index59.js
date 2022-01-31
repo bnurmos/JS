514b701af606d5f8df19d6ee686fef2a87d2c0bf
bnurmos <yuliaslinc@gmail.com>
2022-01-30 17:40:36 -0700

    Created Ex_59

---
+++ index59.js
@@ -0,0 +1,10 @@
+let firstNumber, secondNumber;
+firstNumber = 10;
+secondNumber = 15;
+if (firstNumber == secondNumber) {
+    console.log("They are the same number")
+} else if (firstNumber > secondNumber) {
+    console.log(`${firstNumber} is bigger than ${secondNumber}`)
+} else {
+    console.log(`${secondNumber} is bigger than ${firstNumber}`)
+}
