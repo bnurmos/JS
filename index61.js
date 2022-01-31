e7cc971553242bf6c607e09ff21442d4e541b247
bnurmos <yuliaslinc@gmail.com>
2022-01-30 17:59:56 -0700

    Created Ex_61

---
+++ index61.js
@@ -0,0 +1,36 @@
+let weekdayNumber = 2;
+if (weekdayNumber = 1) {
+    console.log("Today it's Monday")
+} else if (weekdayNumber = 2) {
+    console.log("Today it's Tuesday")
+} else if (weekdayNumber = 3) {
+    console.log("Today it's Wednesday")
+} else if (weekdayNumber = 4) {
+    console.log("Today it's Thursday")
+} else if (weekdayNumber = 5) {
+    console.log("Today it's Friday")
+} else if (weekdayNumber = 6) {
+    console.log("Today it's Saturday")
+} else if (weekdayNumber = 7) {
+    console.log("Today it's Sunday")
+}    else {
+    console.log("Error: Please input a number bettwen 1 and 7")
+}
+
+// let message = null;
+// switch (name) {
+//     case 'Miriam':
+//       message = 'The name is Miriam';
+//       break;
+//     case 'Felipa':
+//       message = 'The name is Felipa';
+//       break;
+//     case 'Xime':
+//       message = 'The name is Xime';
+//       break;
+//     case 'Belu':
+//       message = 'The name is Belu';
+//       break;
+//     default:
+//       message = 'The name is not Marta, Felipa, Xime or Belu';
+//   }
