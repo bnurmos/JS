28d76bceec29fc3210c56f12acc65a8644417f65
bnurmos <yuliaslinc@gmail.com>
2022-01-31 00:08:14 -0700

    Created Ex_76-78

---
+++ index76.js
@@ -0,0 +1,29 @@
+// //Ex_66
+let numbers = 0
+do {
+    console.log(numbers)
+    numbers ++;
+} while (numbers<11);
+
+// //Ex_67
+
+let numbers2 = 0
+do {
+    console.log(numbers2)
+    numbers2 ++;
+} while (numbers2<101);
+
+// Ex_68
+
+var i = 0; 
+do { 
+   console.log(i); 
+   i += 2; 
+} while(i <= 100);
+
+// let number = 1000;
+
+// do {
+//   console.log('number: ', number);
+//   number++;
+// } while (number < 10) ;
