2a849010f34a99906d62c804f7bb81baab1a807c
bnurmos <yuliaslinc@gmail.com>
2022-01-31 20:15:22 -0700

    Created Ex_97

---
+++ index97.js
@@ -0,0 +1,8 @@
+
+for (let number = 10000; number >=0; number--) {
+    if (number % 10 == 0 ) {
+    console.log('number: ** ', number, '**');
+  } else {
+    console.log('number: ', number);
+  }
+}
