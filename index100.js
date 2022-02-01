3f478b89dd10d69f2bfa91ce9bcd60bce9d95fe9
bnurmos <yuliaslinc@gmail.com>
2022-01-31 21:30:05 -0700

    Created Ex_100

---
+++ index100.js
@@ -0,0 +1,17 @@
+function evenNum(num){
+    for (i=0; i<=num;i++){
+        if (i%2==0){
+            console.log(i);
+        }
+    }
+}
+evenNum(100);
+
+// const evenNum = function (num) {
+//     for (i = 0; i <= num; i++) {
+//         if (i % 2 == 0) {
+//             console.log(i);
+//         }
+//     }
+// }
+// evenNum(100);
---
+++ index99.js
@@ -0,0 +1,7 @@
+for (let number = 0; number <=1000; number++) {
+    
+    if (number % 2 == 0) {
+    console.log(number);
+    } else {
+    break; 
+}}
