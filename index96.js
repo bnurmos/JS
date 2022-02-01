d3eec94603e12d5eee92dced4df1f93cc78d8296
bnurmos <yuliaslinc@gmail.com>
2022-01-31 20:06:34 -0700

    Created Ex_96

---
+++ index96.js
@@ -0,0 +1,17 @@
+// for (var current = 20; ; current++) {
+//     if (current % 7 == 0)
+//       break;
+//   }
+//   console.log(current);
+//   // → 21
+
+// for (let i=0; i<=100; i+10){
+//     console.log(i)
+    
+// }
+
+for (let number = 0; number <= 1000; number++) {
+    if (number % 10 == 0) {
+    console.log('number: ', number);
+  }
+}
