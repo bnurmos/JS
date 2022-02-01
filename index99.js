5eb53208f59c9ca5794f20147647a95fcfce1176
bnurmos <yuliaslinc@gmail.com>
2022-02-01 13:44:05 -0700

    Created Ex_99

--- index99.js
+++ index99.js
@@ -1,7 +1,14 @@
-for (let number = 0; number <=1000; number++) {
-    
-    if (number % 2 == 0) {
-    console.log(number);
-    } else {
-    break; 
-}}
+// Add all even numbers bettwen 0 and 1000 using for statement
+// The iteration must break after finding the first 20 even numbers
+// Show the result as output
+
+
+for (let number=0, step = 0; number<=1000; number++) {
+    if (number%2 == 0){
+    console.log(number); 
+    step++;
+    }  else if (step >= 20){
+        break;
+    }
+
+    }
