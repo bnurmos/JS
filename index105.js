9bd8c3a67c1313fa6ea130d0ebe714158760a456
bnurmos <yuliaslinc@gmail.com>
2022-02-01 16:03:15 -0700

    Created Ex_105

---
+++ index105.js
@@ -0,0 +1,11 @@
+// Create a new index105.js file
+// Define a meaningOfLife function
+// The meaningOfLife returns the number 42
+// Show the following message as output: The meaning of life is: %meaningOfLife% (use a template)
+// In this exercise you need to call the meaningOfLife() function inside the template placeholder
+
+meaningOfLife = function(){
+    return 42;
+}
+console.log(`The meaning of life is: ${meaningOfLife()}`)
+meaningOfLife();
