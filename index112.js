9b499ba233109b4f1d2973bd1efb054ce0697174
bnurmos <yuliaslinc@gmail.com>
2022-02-01 21:44:08 -0700

    Created  Ex_112

---
+++ index112.js
@@ -0,0 +1,14 @@
+function showNumbers(start, end) {
+
+    if (start > end) {
+        for (i = start; i >= end; i--) {
+            console.log(i);
+        } 
+    } else {
+        for (i = start; i <= end; i++) {
+            console.log(i);
+        }
+    }
+    
+}
+showNumbers("j", "zdfgf");
