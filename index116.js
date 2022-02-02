04a20ea84aa9926b14edd1f2b79b6c9cce2cf632
bnurmos <yuliaslinc@gmail.com>
2022-02-01 22:27:20 -0700

    Created  Ex_116

---
+++ index116.js
@@ -0,0 +1,25 @@
+function getHexaColor(color){
+    hexa = null;
+    switch(color){
+        case "white":
+        hexa= "#FFFFFF";
+        break;
+        case "black":
+        hexa= "#000000";
+        break;
+        case "blue":
+        hexa= "#0b24fb";
+        break;
+        case "green":
+        hexa= "#0e7e12";
+        break;
+        case "yellow":
+        hexa= "#fffd38";
+        break;
+        case "pink":
+        hexa= "#fec1cc";
+        break;
+} console.log(hexa);
+}
+getHexaColor("pink");
+
