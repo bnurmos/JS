787bad8f75c46b0919ab03f1bbab1b81f9efb33e
bnurmos <yuliaslinc@gmail.com>
2022-02-01 22:17:19 -0700

    Created  Ex_115

---
+++ index115.js
@@ -0,0 +1,9 @@
+function getLongerText(str1,str2){
+    if (str1.length > str2.length){
+        console.log(str1);
+    } else {
+        console.log(str2);
+    } 
+    
+}
+getLongerText("jsgsdfdssff", "hjsfdgs")
