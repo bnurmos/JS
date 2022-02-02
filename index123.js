599742286f034341c8640251fd70e93dd23de02c
bnurmos <yuliaslinc@gmail.com>
2022-02-02 14:20:43 -0700

    Created  Ex_123-124

---
+++ index123.js
@@ -0,0 +1,13 @@
+//Ex_123
+let students = ["Ola", "Nock", "Nick", "Petro", "Huan", "Chen", "Dunkan"];
+numOfStudents=students.length;
+students[1]=students[1].toUpperCase();
+students[5]=students[5].toUpperCase();
+console.log(students);
+console.log(`The students array has ${students.length} items`);
+
+//Ex_124
+
+let movies = ["Alladin", "Shurik", "Ariel", "Rif"];
+console.log(movies);
+console.log(movies[movies.length-1]);
