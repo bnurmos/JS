ef9eb6c1005e10e730acebf02fecc6c6697a0cef
bnurmos <yuliaslinc@gmail.com>
2022-02-02 18:20:59 -0700

    Created Ex_137

---
+++ index137.js
@@ -0,0 +1,7 @@
+let numbers = [5, 4, 2, 6, 9, 8, 7, 1111, 222, 555, 348];
+const numOrigin = numbers.map(function (num) {
+    console.log("Index", numbers.indexOf(num))
+    console.log("Origin", num);
+    console.log("Increm", num+10)
+});
+  //console.log("Index:", )
