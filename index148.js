a8012082b8d365e202abf3c3db95ab0c9e26ffdb
bnurmos <yuliaslinc@gmail.com>
2022-02-03 13:41:44 -0700

    Created Ex_148

---
+++ index148.js
@@ -0,0 +1,12 @@
+let superhero = 
+{
+    name: "Batman",
+    secterName: "Bruce",
+    sideKick: ['Robin', 'Alfred', 'Gordon'],
+    strength: 70
+}
+console.log(superhero);
+console.log(superhero.strength);
+
+superhero.speed=80;
+console.log(superhero);
