7bdb7eb39770102ccc7d5796b9768c1df2e8a956
bnurmos <yuliaslinc@gmail.com>
2022-02-03 14:09:05 -0700

    Created Ex_150

---
+++ index150.js
@@ -0,0 +1,21 @@
+let deepThought = {
+    name: "Deep Thought",
+    answerToTheUltimateQuestionOfLife: function() {
+        console.log(42);
+    }
+}
+deepThought.answerToTheUltimateQuestionOfLife();
+console.log(deepThought.answerToTheUltimateQuestionOfLife);
+
+//Ex_151
+
+let groot = {
+    greet: function(){
+        console.log("I'm Groot!!");
+    },
+    speak: function(){
+        console.log("I'm Groot!!");
+    }
+}
+groot.greet();
+groot.speak();
