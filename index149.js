49adf14da2f3fb0c5d2c009ccd6440ab89c408c5
bnurmos <yuliaslinc@gmail.com>
2022-02-03 13:56:33 -0700

    Created Ex_149

---
+++ index149.js
@@ -0,0 +1,11 @@
+let user = {
+    name: "Vasile",
+    lastName: "Cupidon",
+    isMarried: true,
+    hobbies : ["sleeping", "drinking", "eating"],
+};
+user.car="beetle";
+user.eyes="brown";
+user.ages= 33;
+console.log(user);
+
