bc395ac069b81e7c3afb72529e5ff4340435638d
bnurmos <yuliaslinc@gmail.com>
2022-02-05 19:50:59 -0700

    Created Ex_156 Add a method showDetail to each game object

---
+++ index155 copy - 156.js
@@ -0,0 +1,51 @@
+// let games = [
+//     name= " ",
+//     price= null,
+//     sold= null,
+//     _console= " "
+// ];
+
+let functia = function (){
+    console.log(`name: ${this.name}`, `\nsold: ${this.sold}`,`\nprice: ${this.price}`,`\nconsole: ${this.console}`);
+}
+const games = [
+     {
+        name: "Crash Bandicoot N. Sane Trilogy",
+        price: 1060,
+        sold: 20,
+        console: "PS4",
+        showDetail: functia
+    },
+    {
+        name: "Lego Marvel Super Heroes",
+        price: 700,
+        sold: 25,
+        console: "XBOX",
+        showDetail: functia
+    },
+    {
+        name: "Gta V",
+        price: 1449,
+        sold: 30,
+        console: "PS4",
+        showDetail: functia
+    },
+    {   name: "Fifa 2017",
+        price: 890,
+        sold: 15,
+        console: "PS4",
+        showDetail: functia
+    }
+];
+// games.forEach((item)=>{
+//     console.log(`Use games item properties
+//     name: ${item.name}
+//     price: ${item.price}
+//     sold: ${item.sold}
+//     console: ${item.console}`);
+// });
+const ffffu = (item)=>{
+    item.showDetail();
+}
+
+games.forEach(ffffu);
