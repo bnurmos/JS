deb0d1d6e5877c217588db0f17f4997832c34c57
bnurmos <yuliaslinc@gmail.com>
2022-02-08 14:49:33 -0700

    Created  Ex_133

---
+++ index133.js
@@ -0,0 +1,12 @@
+//Ex_133 - didn't compliet
+const mutants = ["Professor X", "Cyclops","Iceman", "Magneto", "Angel", "Beast","Phoenix", "Logan","Gambit"];
+
+for(var i=0, len=mutants.length; i < len; i++){
+   // console.log(mutants);
+} if (mutants.indexOf('Magneto') > -1){
+    break;
+} else{
+    console.log(mutants);
+}
+const magneto = mutants.indexOf('Magneto');
+//console.log(magneto);
