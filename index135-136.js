81d12e143a245aa5d28424f608bae11780f0d24d
bnurmos <yuliaslinc@gmail.com>
2022-02-08 14:49:18 -0700

    Created  Ex_135-136

---
+++ index135-136.js
@@ -0,0 +1,22 @@
+//Ex_135
+// const pets = ['Amelia', 'Ciro', 'Ulises', 'Carlos'];
+
+// pets.forEach(function(pet, index) {
+//   console.log('index', index); 
+//   console.log(pet);
+// });
+
+// const mutants = ["Professor X", "Cyclops","Iceman", "Magneto", "Angel", "Beast","Phoenix", "Logan","Gambit"];
+// mutants.forEach(function(mutant, index) {
+//       console.log('index', index, mutant); 
+//     });
+
+    //Ex_136  Didn't finish - don't undersand connection between array and function
+// +Iterate over each mutants item using forEach
+// -The objective is to create a string value with Iceman, Logan and Phoenix
+// -To create the string you need to get the names while iterating the mutants array
+// -Once the iteration is over show the generated string with the selected mutants names
+const mutants = ["Professor X", "Cyclops","Iceman", "Magneto", "Angel", "Beast","Phoenix", "Logan","Gambit"];
+mutants.forEach(function(mutant){
+    console.log(mutant)
+});
