a0bdba68a6fef7d23bf24025bcdffd0f990cad9d
bnurmos <yuliaslinc@gmail.com>
2022-02-08 14:48:46 -0700

    Created  Ex_147

---
+++ index147.js
@@ -0,0 +1,16 @@
+let user={
+    userName: "batman",
+    password: "Alfred1960KPO",
+}
+
+function shouldOpenBatCave(user){
+if (userName=="batman"&& password=="Alfred1960KPO"){
+    return true;
+} else {
+    return false;
+}}
+// if (user==shouldOpenBatCave(user)){
+//     console.log("Welcome back batman!!")
+// } else {
+//     console.log("Sorry, you can't enter the Batcave, try again..")
+// }
